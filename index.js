f4111e9573af7f147b16d005e7c5fabe1c1e2466
Nicolas Gualteros <gualteros.herrera.nicolas@gmail.com>
2022-08-22 23:43:47 -0500

    se añadio funcion de desencriptar

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 const sectionEncriptado = document.getElementById("encriptado")
 
-const hola = document.getElementById("hola")
-
-const elemento = `<div class="texto-encriptado">
+const elemento = `<div  class="texto-encriptado">
 <textarea name="" id="textoEncriptado" cols="30" rows="10" placeholder="Texto encriptado" disabled></textarea>
 </div>
 <button id="botonCopiar" class="copiar-texto" onclick="copiarTexto()">Copiar</button>`
 
 document.getElementById("botonEncriptar").addEventListener("click", () => {
     let texto = document.getElementById("textoSinEncriptar").value
-    console.log(texto)
     if (texto !== "") {
         let textoEncriptado=[]
         letras = texto.split("")
-        console.log(letras)
         letras.forEach(element => {
             letraEncriptada= encriptar(element)
             textoEncriptado.push(letraEncriptada)
         });
         textoEncriptado = textoEncriptado.join("")
-        sectionEncriptado.insertAdjacentHTML('beforeend',elemento)
-        console.log(textoEncriptado)
-        const textAreaEncriptado = document.getElementById("textoEncriptado")
-        textAreaEncriptado.value = textoEncriptado
-        return false
-        
+        let cuadroTextoEncriptado = document.getElementById("textoEncriptado")
+        if (!cuadroTextoEncriptado){
+            sectionEncriptado.insertAdjacentHTML('beforeend',elemento)
+        }
+        cuadroTextoEncriptado = document.getElementById("textoEncriptado")
+        cuadroTextoEncriptado.value = textoEncriptado
+        return false            
     }
     alert("escriba algo pendejo")
+})
+
+document.getElementById("botonDesencriptar").addEventListener("click", () => {
+    let texto = document.getElementById("textoSinEncriptar").value
+    if (texto !== "") {
+        let textoEncriptado=[]
+        letras = texto.split(" ")
+        letras.forEach(element => {
+            letraEncriptada= desencriptar(element)
+            textoEncriptado.push(letraEncriptada)
+        });
+        console.log(textoEncriptado)
+        textoEncriptado = textoEncriptado.join(" ")
+        let cuadroTextoEncriptado = document.getElementById("textoEncriptado")
+        if (!cuadroTextoEncriptado){
+            sectionEncriptado.insertAdjacentHTML('beforeend',elemento)
+        }
+        cuadroTextoEncriptado = document.getElementById("textoEncriptado")
+        cuadroTextoEncriptado.value = textoEncriptado
+        return false     
+    }
+    alert("escriba algo pendejo")
+
+
 })
 
 function encriptar (texto) {
@@ -37,11 +58,20 @@ function encriptar (texto) {
                             .replace(/o/, 'ober')
                             .replace(/u/, 'ufat')
 
-    console.log(textoEncriptado)
     return textoEncriptado
+}
+function desencriptar(texto) {
+    textoDescifrado = texto.replace('enter', 'e')
+                            .replace('imes', 'i')
+                            .replace('ai', 'a')
+                            .replace('ober', 'o')
+                            .replace('ufat', 'u')
+
+    return textoDescifrado
 }
 
 function copiarTexto () {
+    const textAreaEncriptado = document.getElementById("textoEncriptado")
     navigator.clipboard.writeText(textAreaEncriptado.value).then(() => {
         console.log("texto copiado")
     })
